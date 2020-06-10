524410b839834f235cb4eff0400117e0c7af4265
Eric <erictomokigoto@gmail.com>
2020-06-09 21:21:37 -0700

    created a simple solution to hide APIKey from github, but API Key would be vulnerable if someone were to get hold of the APK

--- app/src/main/java/com/example/stonks/Data.java
+++ app/src/main/java/com/example/stonks/Data.java
@@ -1,6 +1,10 @@
 package com.example.stonks;
 
 public class Data {
+    private String APIKey;
 
-
+    public Data() {
+        APIKey key= new APIKey();
+        APIKey=key.getAPIKey();
+    }
 }
